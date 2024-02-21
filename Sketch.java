8949193794204ce5e56475d048a6d769d7aff5bc
Justin Mui <justin.mui25@ycdsbk12.ca>
2024-02-21 14:09:46 -0500

    done

--- Sketch.java
+++ Sketch.java
@@ -25,11 +25,32 @@ public class Sketch extends PApplet {
   public void draw() {
 	  
 	// sample code, delete this stuff
-    stroke(128);
-    line(150, 25, 270, 350);  
 
-    stroke(255);
-    line(50, 125, 70, 50);  
+    //Draw Head
+    fill(255,255,0);
+    ellipse(200, 80, 100, 100);
+
+    //Draw Body
+    fill(0);
+    rect(195,130,10,100);
+
+    //Draw Legs
+    stroke(0);
+    line(200,230,150,300);
+    line(200,230,250,300);
+
+    //Draw Arms
+    line(200,180,150,200);
+    line(200,180,250,150);
+
+    //Draw Sun
+    fill(255,255,0);
+    ellipse(0,0,100,100);
+
+    //Foreground grass
+    fill(0,200,0);
+    rect(0,300,400,100);
+
   }
   
   // define other methods down here.
